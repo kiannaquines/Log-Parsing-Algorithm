285a490eda972624691f39c44883ade0348c4dd8
kiannaquines <kjgnaquines@gmail.com>
2024-07-09 15:55:06 +0800

    remove auto_commit.bat branch input

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,12 +1,10 @@
 @echo off
 
 set /p message="Enter commit message: "
-set /p branch="Enter your branch: "
-
 
 git add .
 git commit -m "%message%"
-git push -u origin %branch%
+git push -u origin master
 
 timeout /t 6 /nobreak
 
